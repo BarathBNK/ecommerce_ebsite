7c351ae2f3546b1d9be62179c58431bcdb397641
BarathBNK <140160906+BarathBNK@users.noreply.github.com>
2023-07-21 15:55:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+Result Skip Results Iframe
+EDIT ON
+const close = document.querySelector(".close");
+const open = document.querySelector(".ham");
+const menu = document.querySelector(".menu");
+close.addEventListener("click", () => {
+  menu.style.visibility = "hidden";
+});
+open.addEventListener("click", () => {
+  menu.style.visibility = "visible";
+});
